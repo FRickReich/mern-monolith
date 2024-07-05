90222b72d3be2bd793c8072a2033921564ab83e0
FRickReich <frickreich@gmail.com>
2024-07-05 19:45:11 +0200

    fix: add sass support to storybook webpack setup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
+import webpack from 'webpack';
 
 const config: StorybookConfig = {
   stories: ["../src/frontend/**/*.mdx", "../src/frontend/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -9,10 +10,39 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    "@storybook/addon-styling-webpack"
   ],
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
+  },
+  webpackFinal: async (storybookConfig: webpack.Configuration) => {
+    // Find and remove the existing rule that handles CSS
+    const cssRuleIndex = storybookConfig.module?.rules.findIndex(rule => {
+      if (rule.test instanceof RegExp) {
+        return rule.test.test('.css');
+      }
+      return false;
+    });
+
+    if (cssRuleIndex !== undefined && cssRuleIndex !== -1) {
+      storybookConfig.module?.rules.splice(cssRuleIndex, 1);
+    }
+
+    // Add SCSS support
+    storybookConfig.module?.rules.push(
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      }
+    );
+
+    // Return the updated Storybook webpack configuration
+    return storybookConfig;
   },
 };
 export default config;
